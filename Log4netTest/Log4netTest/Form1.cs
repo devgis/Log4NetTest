b9fa626c396488371f4feebbd8ba351c95e94886
devgis <devgis@163.com>
2022-05-11 13:17:39 +0800

    Backuped
    Backuped

---
+++ Log4netTest/Log4netTest/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Log4netTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LogHelper.WriteLog(typeof(Form1), "测试Log4Net日志是否写入");
+        }
+    }
+}
